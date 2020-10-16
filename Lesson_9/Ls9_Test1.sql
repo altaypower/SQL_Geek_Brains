a87392f66de6098c8e39be25d6c987bb518ae09d
altaypower <altaypower@gmail.com>
2020-10-16 13:43:04 +0700

    file Ls9_Test1

---
+++ Lesson_9/Ls9_Test1.sql
@@ -0,0 +1,40 @@
+-- В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. 
+-- Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
+
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Тимофей', '1992-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+  
+ use shop
+ 
+ select * from shop.users where `id` = 1;
+
+START TRANSACTION;
+
+insert into sample.users select * from shop.users where `id` = 1;
+
+select * from sample.users;
+
+COMMIT;
+
+
+
+
+
+
+ 
+ 
